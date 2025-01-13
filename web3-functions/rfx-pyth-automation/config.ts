4dbb18ec42619ce1f80219da39254cf8ba948dd8
vedant <vedant@neobase.one>
2025-01-13 16:18:28 +0530

    add delay

--- web3-functions/rfx-pyth-automation/config.ts
+++ web3-functions/rfx-pyth-automation/config.ts
@@ -177,5 +177,3 @@ export const feedIdCache: Record<string, string> = {
 export const baseFeedIdCache: Record<string, string> = {
   "0x2dEd5585e0bE28bb0735D01c403F064a8986d2B9" : "0xc9d8b075a5c69303365ae23633d4e085199bf5c520a3b90fed1322a0342ffc33", // ETH/BTC
 }
-
-
