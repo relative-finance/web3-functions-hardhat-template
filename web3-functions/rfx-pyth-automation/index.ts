910c54d55766e8f1dbd31cf8b00299723b8f7a7e
hridyansh07 <62767060+hridyansh07@users.noreply.github.com>
2025-03-19 17:11:36 +0530

    Added SDK for Oracle Params + Removed Pyth Specific Functionality (#5)
    * Added SDK for Oracle Params + Removed Pyth Specific Functionality
    
    * Removed / from URL
    
    * Added Mainnet Market Config
    
    * Uncomment Mainnet Config for Order Handlers
    
    * Removed FeedId/BaseFeedId Cache from config
    
    * Updated URLs + Fixed Type Errors

--- web3-functions/rfx-pyth-automation/index.ts
+++ web3-functions/rfx-pyth-automation/index.ts
@@ -3,22 +3,26 @@ import {
   Web3Function,
   Web3FunctionEventContext,
 } from "@gelatonetwork/web3-functions-sdk";
-
-import EventEmitterAbi from './abis/EventEmitter.json';
-import OrderHandlerAbi from './abis/OrderHandler.json';
-import DepositHandlerAbi from './abis/DepositHandler.json';
-import WithdrawalHandlerAbi from './abis/WithdrawalHandler.json';
-
-import PythAbi from "@pythnetwork/pyth-sdk-solidity/abis/IPyth.json";
-
-import { decodeEventData, getOracleParams, sleep } from "./utils"
-import { MarketProps, marketProps, config } from "./config"
+import EventEmitterAbi from "./abis/EventEmitter.json";
+import OrderHandlerAbi from "./abis/OrderHandler.json";
+import DepositHandlerAbi from "./abis/DepositHandler.json";
+import WithdrawalHandlerAbi from "./abis/WithdrawalHandler.json";
+import { decodeEventData, getOracleParamsSDKBulk, sleep } from "./utils";
+import { MarketProps, marketProps, config } from "./config";
 import { Contract } from "ethers";
+import { KyInstance } from "ky/distribution/types/ky";
+import ky from "ky";
+
+let SDK_API: KyInstance;
 
 Web3Function.onRun(async (context: Web3FunctionEventContext) => {
-  const { log, secrets, multiChainProvider } = context;
+  const { log, multiChainProvider } = context;
 
-  const HERMES_ENDPOINT = await secrets.get("HERMES_ENDPOINT");
+  console.log(log);
+  SDK_API = ky.create({
+    prefixUrl: "https://ccgv6da97e.execute-api.us-east-1.amazonaws.com", // Mainnet URL
+    // prefixUrl: "https://i3t32kr8e2.execute-api.us-east-1.amazonaws.com", // Internal URL
+  });
 
   try {
     // Parse the event from the log using the provided event ABI
@@ -31,11 +35,14 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
       event.args[1] != "OrderCreated" &&
       event.args[1] != "WithdrawalCreated"
     ) {
-      console.log(`event found: ${event.args[1]}, ignoring`)
-      return { canExec: false, message: `event found: ${event.args[1]}, ignoring`};
+      console.log(`event found: ${event.args[1]}, ignoring`);
+      return {
+        canExec: false,
+        message: `event found: ${event.args[1]}, ignoring`,
+      };
     }
 
-    console.log(`event found: ${event.args[1]}`)
+    console.log(`event found: ${event.args[1]}`);
     const {
       key,
       market,
@@ -51,8 +58,11 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
       orderType != 2 &&
       orderType != 4
     ) {
-      console.log(`order type found: ${orderType}, ignoring`)
-      return { canExec: false, message: `order type found: ${orderType}, ignoring`};
+      console.log(`order type found: ${orderType}, ignoring`);
+      return {
+        canExec: false,
+        message: `order type found: ${orderType}, ignoring`,
+      };
     }
 
     console.log(`order tx: ${log.transactionHash}`);
@@ -60,8 +70,8 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
     // sleep 2 seconds to reduce chances of w3f passing simulation when other automation executes the item
     await sleep(2 * 1000);
 
-    let props: MarketProps[] = [];
-    props.push(marketProps[market])
+    const props: MarketProps[] = [];
+    props.push(marketProps[market]);
 
     for (let i = 0; i < longTokenSwapPath.length; i++) {
       props.push(marketProps[longTokenSwapPath[i]]);
@@ -74,15 +84,11 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
     }
 
     const provider = multiChainProvider.default();
-    
-    const pythContract = new Contract(
-      config.PythOracle,
-      PythAbi,
-      provider
-    );
 
-    const blockTimestamp = (await provider.getBlock(log.blockNumber)).timestamp;
-    const { oracleParams, updateFee } = await getOracleParams(props, blockTimestamp, pythContract, HERMES_ENDPOINT!);
+    const { oracleParams, updateFee } = await getOracleParamsSDKBulk(
+      SDK_API,
+      props
+    );
 
     if (event.args[1] == "DepositCreated") {
       const depositHandler = new Contract(
@@ -92,11 +98,18 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
       );
       return {
         canExec: true,
-        callData: [{to: depositHandler.address, data: depositHandler.interface.encodeFunctionData("executeDeposit", [key, oracleParams]), value: updateFee.toString()}]
-      }
-    }
-    
-    else if (event.args[1] == "OrderCreated") {
+        callData: [
+          {
+            to: depositHandler.address,
+            data: depositHandler.interface.encodeFunctionData(
+              "executeDeposit",
+              [key, oracleParams]
+            ),
+            value: updateFee.toString(),
+          },
+        ],
+      };
+    } else if (event.args[1] == "OrderCreated") {
       const orderHandler = new Contract(
         config.OrderHandler,
         OrderHandlerAbi,
@@ -104,11 +117,18 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
       );
       return {
         canExec: true,
-        callData: [{to: orderHandler.address, data: orderHandler.interface.encodeFunctionData("executeOrder", [key, oracleParams]), value: updateFee.toString()}]
-      }
-    }
-    
-    else if (event.args[1] == "WithdrawalCreated") {
+        callData: [
+          {
+            to: orderHandler.address,
+            data: orderHandler.interface.encodeFunctionData("executeOrder", [
+              key,
+              oracleParams,
+            ]),
+            value: updateFee.toString(),
+          },
+        ],
+      };
+    } else if (event.args[1] == "WithdrawalCreated") {
       const withdrawalHandler = new Contract(
         config.WithdrawalHandler,
         WithdrawalHandlerAbi,
@@ -116,16 +136,25 @@ Web3Function.onRun(async (context: Web3FunctionEventContext) => {
       );
       return {
         canExec: true,
-        callData: [{to: withdrawalHandler.address, data: withdrawalHandler.interface.encodeFunctionData("executeWithdrawal", [key, oracleParams]), value: updateFee.toString()}]
-      }
+        callData: [
+          {
+            to: withdrawalHandler.address,
+            data: withdrawalHandler.interface.encodeFunctionData(
+              "executeWithdrawal",
+              [key, oracleParams]
+            ),
+            value: updateFee.toString(),
+          },
+        ],
+      };
     }
 
     return {
       canExec: false,
-      message: `no match: ${event.args[1]}`
+      message: `no match: ${event.args[1]}`,
     };
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return {
       canExec: false,
       message: (err as Error).message,
